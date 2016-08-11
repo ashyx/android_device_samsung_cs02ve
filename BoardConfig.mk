531a720e250324e56016061c52fa0c46208c722d
santeri3700 <santeri.pikarinen@gmail.com>
2016-08-11 21:40:32 +0300

    Fixed some platform information

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,27 +5,30 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+# Uncomment if needed!
+#TARGET_BOARD_PLATFORM_GPU := qcom-adreno302
+TARGET_BOARD_PLATFORM := msm8610
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := cs02ve3gdtv
+TARGET_BOOTLOADER_BOARD_NAME := MSM8610
 
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F
-BOARD_KERNEL_BASE := 0x00000000
+#BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F
+#BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-# fix this up by examining /proc/mtd on a running device
+# Waiting for sizes. Not valid atm
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+# We are using the SM-G3502T prebuilt kernel for now.
 TARGET_PREBUILT_KERNEL := device/samsung/cs02ve3gdtv/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true

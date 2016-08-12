05772cffefd24168230678965964f81dbfd8c095
santeri3700 <santeri.pikarinen@gmail.com>
2016-08-12 16:56:26 +0300

    Fixed product info

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := cs02ve3gdtv
+PRODUCT_RELEASE_NAME := cs02ve
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/cs02ve3gdtv/device_cs02ve3gdtv.mk)
+$(call inherit-product, device/samsung/cs02ve/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := cs02ve3gdtv
-PRODUCT_NAME := cm_cs02ve3gdtv
+PRODUCT_DEVICE := cs02ve
+PRODUCT_NAME := cm_cs02ve
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := cs02ve3gdtv
+PRODUCT_MODEL := cs02ve
 PRODUCT_MANUFACTURER := samsung

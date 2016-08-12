80d22cf16e5b11ec4ee576bd2a7c39ba48f1d262
santeri3700 <santeri.pikarinen@gmail.com>
2016-08-12 16:54:35 +0300

    Fixed product info

--- device.mk
+++ device.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/cs02ve3gdtv/cs02ve3gdtv-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/cs02ve/cs02ve-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/cs02ve3gdtv/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/cs02ve/overlay
 
-LOCAL_PATH := device/samsung/cs02ve3gdtv
+LOCAL_PATH := device/samsung/cs02ve
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_cs02ve3gdtv
-PRODUCT_DEVICE := cs02ve3gdtv
+PRODUCT_NAME := full_cs02ve
+PRODUCT_DEVICE := cs02ve

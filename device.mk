4611d7d98446b3d0cf8d7bf5acca726b836438b8
Tim Schumacher <TimSchumi@users.noreply.github.com>
2016-08-16 11:39:38 +0200

    Added twrp.fstab to PRODUCT_COPY_FILES

--- device.mk
+++ device.mk
@@ -17,6 +17,8 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += device/samsung/cs02ve/twrp.fstab:recovery/root/etc/twrp.fstab
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
